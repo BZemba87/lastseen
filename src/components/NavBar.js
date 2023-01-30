ab2fb006c9cbf460dae21e1f8e1938a8c6540534
BZemba87 <zembabernadette@gmail.com>
2023-01-30 04:04:21 +0000

    connect react app to api on Heroku

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,42 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
+import styles from "../styles/NavBar.module.css";
+import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
-  return(
-    <Navbar expand="md" fixed="top">
+  return (
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
-        <Navbar.Brand>LastSeen</Navbar.Brand>
+        <NavLink to="/">
+          <Navbar.Brand>
+            LastSeen
+          </Navbar.Brand>
+        </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-left">
-            <Nav.Link>
-              <i className="fas fa-home"></i> Home
-            </Nav.Link>
-            <Nav.Link>Login</Nav.Link>
-            <Nav.Link>Sign Up</Nav.Link>
+            <NavLink
+              exact
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/"
+            >
+              <i className="fas fa-home"></i>Home
+            </NavLink>
+            <NavLink
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/signin"
+            >
+              <i className="fas fa-sign-in-alt"></i>Login
+            </NavLink>
+            <NavLink
+              to="/login"
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+            >
+              <i className="fas fa-user-plus"></i>Sign up
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </Container>
