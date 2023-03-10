b7d168d007b4a3cfa035cb96fe90dea5ab0dce26
BZemba87 <zembabernadette@gmail.com>
2023-03-10 16:05:59 +0000

    add handling events to LogInForm.js

--- src/pages/auth/LogInForm.js
+++ src/pages/auth/LogInForm.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios"
 
 import Form from "react-bootstrap/Form";
 import Alert from "react-bootstrap/Alert";
@@ -8,21 +9,41 @@ import Row from "react-bootstrap/Row";
 import Image from "react-bootstrap/Image";
 import Container from "react-bootstrap/Container";
 
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 import styles from "../../styles/SignUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 
 function LogInForm() {
-//   Add your component logic here
+  const [signInData, setSignInData] = useState({
+  username: "",
+  password: "",
+});
+const { username, password } = signInData;
+
+const history = useHistory();
+const handleSubmit = async (event) => {
+  event.preventDefault();
+  try {
+    await axios.post("/dj-rest-auth/login/", signInData);
+    history.push("/");
+  } catch (err) {
+  }
+};
+const handleChange = (event) => {
+  setSignInData({
+    ...signInData,
+    [event.target.name]: event.target.value,
+  });
+};
 
   return (
     <Row className={styles.Row}>
       <Col className="my-auto p-0 p-md-2" md={6}>
         <Container className={`${appStyles.Content} p-4 `}>
           <h1 className={styles.Header}>log in</h1>
-          <Form>
+          <Form onSubmit={handleSubmit}>
                 <Form.Group controlId="username">
                     <Form.Label className="d-none">Username</Form.Label>
                     <Form.Control 
@@ -30,6 +51,8 @@ function LogInForm() {
                         placeholder="Username"
                         name="username" 
                         className={styles.Input}
+                        value={username}
+                        onChange={handleChange}
                     />
                 </Form.Group>
 
@@ -40,6 +63,8 @@ function LogInForm() {
                         placeholder="Password" 
                         name="password"
                         className={styles.Input}
+                        value={password}
+                        onChange={handleChange}
                     />
                 </Form.Group>
                 <Button 
@@ -52,7 +77,7 @@ function LogInForm() {
         </Container>
         <Container className={`mt-3 ${appStyles.Content}`}>
           <Link className={styles.Link} to="/signup">
-            Don't have an account? <span>Sign up now!</span>
+            Want to be seen? <span>Sign up now!</span>
           </Link>
         </Container>
       </Col>
