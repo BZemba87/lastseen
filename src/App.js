98f81538bf78d70f74e7cd36df807729863b275b
BZemba87 <zembabernadette@gmail.com>
2023-03-18 23:16:41 +0000

    create PostsPage component and add routes for feed and liked pages in App.js

--- src/App.js
+++ src/App.js
@@ -7,14 +7,44 @@ import SignUpForm from './pages/auth/SignUpForm';
 import LogInForm from './pages/auth/LogInForm';
 import PostCreateForm from './pages/posts/PostCreateForm';
 import PostPage from './pages/posts/PostPage';
+import PostsPage from './pages/posts/PostsPage';
+import { useCurrentUser } from './contexts/CurrentUserContexts';
 
 function App() {
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || "";
+
+
   return (
         <div className={styles.App}>
           <NavBar />
           <Container className={styles.Main}>
             <Switch>
-              <Route exact path="/" render={() => <h1>Home page</h1>} />
+              <Route 
+                exact 
+                path="/" 
+                render={() => (
+                 <PostsPage message="Nothing to see here!  Search again." />
+                )}
+              />
+              <Route 
+                exact 
+                path="/feed" 
+                render={() => (
+                 <PostsPage message="Nothing to see here!  Search again or follow a friend." 
+                 filter={`owner__followed__owner__profile=${profile_id}&`}
+                 />
+                )}
+                />
+              <Route 
+                exact 
+                path="/liked" 
+                render={() => (
+                 <PostsPage message="Nothing to see here!  Search again or like a post." 
+                 filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}
+                 />
+                )}
+              />
               <Route exact path="/login" render={() => <LogInForm />} />
               <Route exact path="/signup" render={() => <SignUpForm />} />
               <Route exact path="/posts/create" render={() => <PostCreateForm />} />
