82b4de6867d53ad0f9a79978b3badb822e021881
BZemba87 <zembabernadette@gmail.com>
2023-03-19 18:27:32 +0000

    install infinite scroll component and set props in PostsPage.js

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -14,6 +14,7 @@ import { useLocation } from "react-router";
 import { axiosReq } from "../../api/axiosDefault";
 
 import NoResults from "../../assets/no-results.png";
+import InfiniteScroll from "react-infinite-scroll-component";
 
 function PostsPage({ message, filter = "" }) {
   const [posts, setPosts] = useState({ results: [] });
@@ -60,9 +61,19 @@ function PostsPage({ message, filter = "" }) {
         {hasLoaded ? (
           <>
             {posts.results.length ? (
-              posts.results.map((post) => (
-                <Post key={post.id} {...post} setPosts={setPosts} />
-              ))
+              <InfiniteScroll
+                children={
+                  posts.results.map((post) => (
+                    <Post key={post.id} {...post} setPosts={setPosts} />
+                  ))
+
+                }
+                dataLength={posts.results.length}
+                loader={<Asset spinner />}
+                hasMore={!!posts.next}
+                next={() => {}}
+              
+              />
             ) : (
               <Container className={appStyles.Content}>
                 <Asset src={NoResults} message={message} />
