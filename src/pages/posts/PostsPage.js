3539364d13911468e0940ea14579aa3581b3d5b9
BZemba87 <zembabernadette@gmail.com>
2023-03-19 16:04:20 +0000

    add text search functionality to PostsPage and timer so posts stop flashing as user types in search bar

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -20,10 +20,12 @@ function PostsPage({ message, filter = "" }) {
   const [hasLoaded, setHasLoaded] = useState(false);
   const { pathname } = useLocation();
 
+  const [query, setQuery] = useState("");
+
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const { data } = await axiosReq.get(`/posts/?${filter}`);
+        const { data } = await axiosReq.get(`/posts/?${filter}search=${query}`);
         setPosts(data);
         setHasLoaded(true);
       } catch (err) {
@@ -32,8 +34,13 @@ function PostsPage({ message, filter = "" }) {
     };
 
     setHasLoaded(false);
-    fetchPosts();
-  }, [filter, pathname]);
+    const timer = setTimeout(() => {
+      fetchPosts();
+    }, 1000)
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [filter, query, pathname]);
 
   return (
     <Row className="h-100">
@@ -43,7 +50,11 @@ function PostsPage({ message, filter = "" }) {
         <Form className={styles.SearchBar}
           onSubmit={(event) => event.PreventDefault()}
           >
-            <Form.Control type="text" className="mr-sm-2"
+            <Form.Control 
+            value={query}
+            onChange={(event) => setQuery(event.target.value)}
+            type="text" 
+            className="mr-sm-2"
             placeholder="Find posts" />
         </Form>
         {hasLoaded ? (
